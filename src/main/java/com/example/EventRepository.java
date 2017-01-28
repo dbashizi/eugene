202c9cb16a6a30bd72d30d28a404c069507431ba
Conor Thomas <m.conorthomas@gmail.com>
2017-01-28 13:22:35 -0500

    tenth commit

--- src/main/java/com/example/EventRepository.java
+++ src/main/java/com/example/EventRepository.java
@@ -3,4 +3,6 @@ package com.example;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EventRepository extends CrudRepository<Event, Integer> {
+    Event findById(Integer id);
+    Event findByName(String name);
 }
--- src/main/java/com/example/JSONController.java
+++ src/main/java/com/example/JSONController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,6 +16,72 @@ public class JSONController {
     @Autowired
     PersonRepository people;
 
+    @Autowired
+    EventRepository events;
+
+    @PostConstruct
+    public void init() {
+        if (people.findByEmail("joe@gmail.com") == null) {
+            Person person = new Person();
+            person.setFirstName("Joe");
+            person.setLastName("Thomas");
+            person.setEmail("joe@gmail.com");
+            person.setCompany("Coke");
+            person.setPosition("sales");
+            person.setSharePicture(false);
+            person.setPassword("password");
+            people.save(person);
+        }
+
+        if (people.findByEmail("carson@gmail.com") == null) {
+            Person person = new Person();
+            person.setFirstName("Carson");
+            person.setLastName("Wentz");
+            person.setEmail("carson@gmail.com");
+            person.setCompany("NFL");
+            person.setPosition("QB");
+            person.setSharePicture(false);
+            person.setPassword("password");
+            people.save(person);
+        }
+
+        if (people.findByEmail("michael@gmail.com") == null) {
+            Person person = new Person();
+            person.setFirstName("Michael");
+            person.setLastName("Jordan");
+            person.setEmail("michael@gmail.com");
+            person.setCompany("NBA");
+            person.setPosition("GOAT");
+            person.setSharePicture(false);
+            person.setPassword("password");
+            people.save(person);
+        }
+
+        if (events.findByName("Test123456") == null) {
+            Event event = new Event();
+            event.setName("Test123456");
+            event.setLocation("The Iron Yard");
+            event.setAddress("MLK Blvd Atlanta, GA");
+            event.setStartTime("Jan 29th @ 4:00pm");
+        }
+
+        if (events.findByName("Test2468") == null) {
+            Event event = new Event();
+            event.setName("Test2468");
+            event.setLocation("ATV");
+            event.setAddress("3700 Piedmont Road NE Atlanta GA");
+            event.setStartTime("Feb 6th @ 4:00pm");
+        }
+
+        if (events.findByName("Test1357") == null) {
+            Event event = new Event();
+            event.setName("Test1357");
+            event.setLocation("Georgia Dome");
+            event.setAddress("MLK Blvd Atlanta, GA");
+            event.setStartTime("Feb 24th 6:00pm");
+        }
+    }
+
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public Person login(@RequestBody RequestLogin requestLogin) throws Exception {
@@ -25,7 +92,7 @@ public class JSONController {
         }
         else if (!requestLogin.getPassword().equals(person.getPassword())) {
             throw new Exception("Incorrect password");
-        } 
+        }
         return person;
     }
 
