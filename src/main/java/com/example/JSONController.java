9e67dbce0ccc4361532144edfcb84f364dc72a0e
Conor Thomas <m.conorthomas@gmail.com>
2017-01-28 11:13:59 -0500

    tenth commit

--- src/main/java/com/example/JSONController.java
+++ src/main/java/com/example/JSONController.java
@@ -19,7 +19,7 @@ public class JSONController {
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public Person login(HttpSession session, String email, String password) throws Exception {
         //HashMap login = new HashMap();
-        Person person = people.findByEmail(email);
+        Person person = people.findByEmail("jon@gmail.com");
         if (person == null) {
             throw  new Exception("that email doesn't exist");
         }
