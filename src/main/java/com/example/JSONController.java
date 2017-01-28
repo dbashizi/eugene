d4edbe614c28e6e5bbc55270b4305d78dd011f40
Conor Thomas <m.conorthomas@gmail.com>
2017-01-27 19:55:58 -0500

    tenth commit

--- src/main/java/com/example/JSONController.java
+++ src/main/java/com/example/JSONController.java
@@ -40,7 +40,7 @@ public class JSONController {
     @RequestMapping(path = "/get_people", method = RequestMethod.GET)
     public List<Person> getPeople() {
         Person personOne = new Person(1, "conor", "thomas", "tiy", "student", "test@gmail.com", "password");
-        Person personTwo = new Person(1, "lynsey", "thomas", "buckhead church", "groups director", "test2@gmail.com", "password");
+        Person personTwo = new Person(2, "lynsey", "thomas", "buckhead church", "groups director", "test2@gmail.com", "password");
 
         List<Person> personList = new ArrayList<>();
         personList.add(personOne);
